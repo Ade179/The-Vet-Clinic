9b1f520044a5f2279a0eff38278023d6132f4189
Juwon <dnladesanya1@gmail.com>
2023-02-24 17:43:07 +0100

    fixed

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals( id integer, name text, date_of_birth date, escape_attempts integer, neutered boolean, weight_kg decimal );
+CREATE TABLE animals( id SERIAL NOT NULL , name text, date_of_birth date, escape_attempts integer, neutered boolean, weight_kg decimal )
+
